727c77d80bceb0ec95b4a892efdf729c864bb967
Matt Bray <mattjbray@gmail.com>
2019-04-14 17:06:10 +0100

    doc: add note about objects in S-expressions

--- src-sexplib/decode.mli
+++ src-sexplib/decode.mli
@@ -1,3 +1,13 @@
 (** Turn S-expressions into Ocaml values via Sexplib. *)
 
+(** Following the convention of [Sexplib0.Sexp_conv.hashtbl_of_sexp], we
+    consider an S-expression to be an "object" if it is a list of two-element
+    lists. For example:
+
+        ((field1 value2) (field2 value2))
+
+    Like YAML, fields of an object are not necessarily atoms. To handle these,
+    look for the primed combinators (e.g. [keys']).
+*)
+
 include Decoders.Decode.S with type value = Sexplib0.Sexp.t
