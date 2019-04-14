5e9718c94f944266c5ea49524fb908724fdeb166
Matt Bray <mattjbray@gmail.com>
2019-04-14 11:30:08 +0100

    test: sexplib

---
+++ src-sexplib/test/main.ml
@@ -0,0 +1,34 @@
+open OUnit2
+
+let sexplib_suite =
+  let open Decoders_sexplib.Decode in
+
+  let decoder_test ~decoder ~input ~expected _test_ctxt =
+    match decode_string decoder input with
+    | Ok value -> assert_equal value expected
+    | Error error -> assert_string (Format.asprintf "%a" pp_error error)
+  in
+
+  "Sexplib" >:::
+  [ "list string" >::
+    decoder_test
+      ~decoder:(list string)
+      ~input:"(hello world)"
+      ~expected:["hello"; "world"]
+  ; "field_opt present" >::
+    decoder_test
+      ~decoder:(field_opt "optional" string)
+      ~input:"((optional hello))"
+      ~expected:(Some "hello")
+  ; "field_opt missing" >::
+    decoder_test
+      ~decoder:(field_opt "optional" string)
+      ~input:"()"
+      ~expected:None
+  ]
+
+let () =
+  "decoders" >:::
+  [ sexplib_suite
+  ]
+  |> run_test_tt_main
