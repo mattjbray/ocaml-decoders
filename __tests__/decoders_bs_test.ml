ec0873da3f0ae66374cc4d8f9e938ec559b914f1
Dave Aitken <dave.aitken@gmail.com>
2020-11-06 11:25:06 +0000

    add bs-specific `array` decoder

--- __tests__/decoders_bs_test.ml
+++ __tests__/decoders_bs_test.ml
@@ -17,6 +17,20 @@ let () =
 
 let () =
   describe
+    "decoders-bs decode array"
+    Expect.(
+      fun () ->
+        test
+          "array"
+          Decode.(
+            fun () ->
+              let json_str = {|["a", "b", "c"]|} in
+              let decoded = decode_string (array string) json_str in
+              expect decoded |> toEqual (Belt.Result.Ok [| "a"; "b"; "c" |])))
+
+
+let () =
+  describe
     "decoders-bs encode"
     Expect.(
       fun () ->
@@ -27,3 +41,17 @@ let () =
               let str = "Hello world" in
               let encoded = encode_string string str in
               expect encoded |> toEqual {|"Hello world"|}))
+
+
+let () =
+  describe
+    "decoders-bs encode array"
+    Expect.(
+      fun () ->
+        test
+          "string"
+          Encode.(
+            fun () ->
+              let x = [| "a"; "b"; "c" |] in
+              let encoded = encode_string (array string) x in
+              expect encoded |> toEqual {|["a","b","c"]|}))
