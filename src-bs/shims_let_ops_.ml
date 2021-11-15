6e96d39048f94ee69104299ae45c92b06cc7a471
Matt Bray <mattjbray@gmail.com>
2021-11-15 18:59:47 +0000

    clean: unused var warnings on ocaml < 4.08

--- src-bs/shims_let_ops_.ml
+++ src-bs/shims_let_ops_.ml
@@ -1,11 +1,18 @@
 (* Note: copied from src/gen/mkshims.ml *)
+module type I = sig
+  type ('i, 'a, 'e) t
+
+  val ( >|= ) : ('i, 'a, 'e) t -> ('a -> 'b) -> ('i, 'b, 'e) t
+
+  val monoid_product : ('i, 'a, 'e) t -> ('i, 'b, 'e) t -> ('i, 'a * 'b, 'e) t
+
+  val ( >>= ) : ('i, 'a, 'e) t -> ('a -> ('i, 'b, 'e) t) -> ('i, 'b, 'e) t
+end
+
 module type S = sig
   type ('i, 'a, 'e) t
 end
 
-module Make (X : sig
-  type ('i, 'a, 'e) t
-end) =
-struct
+module Make (X : I) = struct
   type ('i, 'a, 'e) t = ('i, 'a, 'e) X.t
 end
--- src/gen/mkshims.ml
+++ src/gen/mkshims.ml
@@ -1,9 +1,21 @@
 (* Note: also copied to src-bs/shims_let_ops_.ml *)
+
+let shims_all =
+  {|
+  module type I = sig
+    type ('i, 'a, 'e) t
+    val (>|=) : ('i, 'a, 'e) t -> ('a -> 'b) -> ('i, 'b, 'e) t
+    val monoid_product : ('i, 'a, 'e) t -> ('i, 'b, 'e) t -> ('i, ('a * 'b), 'e) t
+    val (>>=) : ('i, 'a, 'e) t -> ('a -> ('i, 'b, 'e) t) -> ('i, 'b, 'e) t
+  end
+|}
+
+
 let shims_let_op_pre_408 =
   {|
   module type S = sig type ('i, 'o, 'e) t end
-  module Make(X:sig type ('i, 'o, 'e) t end)
-    : S with type ('i, 'o, 'e) t = ('i, 'o, 'e) X.t  = struct
+  module Make(X : I) : S with type ('i, 'o, 'e) t = ('i, 'o, 'e) X.t =
+  struct
     type ('i, 'o, 'e) t = ('i, 'o, 'e) X.t
   end
 |}
@@ -19,12 +31,8 @@ let shims_let_op_post_408 =
     val ( and* ) : ('i, 'a, 'e) t -> ('i, 'b, 'e) t -> ('i, 'a * 'b, 'e) t
   end
 
-  module Make(X:sig
-    type ('i, 'a, 'e) t
-    val (>|=) : ('i, 'a, 'e) t -> ('a -> 'b) -> ('i, 'b, 'e) t
-    val monoid_product : ('i, 'a, 'e) t -> ('i, 'b, 'e) t -> ('i, ('a * 'b), 'e) t
-    val (>>=) : ('i, 'a, 'e) t -> ('a -> ('i, 'b, 'e) t) -> ('i, 'b, 'e) t
-  end) : S with type ('i, 'o, 'e) t = ('i, 'o, 'e) X.t = struct
+  module Make(X : I) : S with type ('i, 'o, 'e) t = ('i, 'o, 'e) X.t =
+  struct
     type ('i, 'o, 'e) t = ('i, 'o, 'e) X.t
     let (let+) = X.(>|=)
     let (and+) = X.monoid_product
@@ -37,6 +45,7 @@ let shims_let_op_post_408 =
 let () =
   let version = Sys.ocaml_version in
   let major, minor = Scanf.sscanf version "%u.%u" (fun maj min -> (maj, min)) in
+  print_endline shims_all ;
   print_endline
     ( if (major, minor) >= (4, 8)
     then shims_let_op_post_408
