84d1948542c8ea4e7e016235759aa93222a111ab
Matt Bray <mattjbray@gmail.com>
2021-11-15 18:22:38 +0000

    chore: Decoder mli

---
+++ src/decoder.mli
@@ -0,0 +1,41 @@
+type ('i, 'o, 'e) t = 'i -> ('o, 'e) result
+
+val pure : 'o -> ('i, 'o, 'e) t
+
+val fail : 'e -> ('i, 'o, 'e) t
+
+val of_result : ('o, 'e) Decoders_util.My_result.t -> ('i, 'o, 'e) t
+
+val bind : ('a -> ('i, 'b, 'e) t) -> ('i, 'a, 'e) t -> ('i, 'b, 'e) t
+
+val map : ('a -> 'b) -> ('i, 'a, 'e) t -> ('i, 'b, 'e) t
+
+val map_err : ('e1 -> 'e2) -> ('i, 'o, 'e1) t -> ('i, 'o, 'e2) t
+
+val apply : ('i, 'a -> 'b, 'e) t -> ('i, 'a, 'e) t -> ('i, 'b, 'e) t
+
+module Infix : sig
+  val ( >>= ) : ('i, 'a, 'e) t -> ('a -> ('i, 'b, 'e) t) -> ('i, 'b, 'e) t
+
+  val ( >|= ) : ('i, 'a, 'e) t -> ('a -> 'b) -> ('i, 'b, 'e) t
+
+  val ( <*> ) : ('i, 'a -> 'b, 'e) t -> ('i, 'a, 'e) t -> ('i, 'b, 'e) t
+
+  include Shims_let_ops_.S with type ('i, 'o, 'e) t = ('i, 'o, 'e) t
+end
+
+val fix : (('i, 'a, 'e) t -> ('i, 'a, 'e) t) -> ('i, 'a, 'e) t
+
+val value : ('i, 'i, 'e) t
+
+val maybe : ('i, 'a, 'e) t -> ('i, 'a option, 'e) t
+
+val one_of :
+  combine_errors:('e list -> 'e) -> ('i, 'o, 'e) t list -> ('i, 'o, 'e) t
+
+val pick :
+     combine_errors:('e list -> 'e)
+  -> ('i, ('i, 'o, 'e) t, 'e) t list
+  -> ('i, 'o, 'e) t
+
+val of_to_opt : ('i -> 'o option) -> ('i -> ('o, 'e) result) -> ('i, 'o, 'e) t
--- src/gen/mkshims.ml
+++ src/gen/mkshims.ml
@@ -1,16 +1,31 @@
 (* Note: also copied to src-bs/shims_let_ops_.ml *)
-let shims_let_op_pre_408 = {|
-   module Make(X:sig  end) = struct  end
+let shims_let_op_pre_408 =
+  {|
+  module type S = sig type ('i, 'o, 'e) t end
+  module Make(X:sig type ('i, 'o, 'e) t end)
+    : S with type ('i, 'o, 'e) t = ('i, 'o, 'e) X.t  = struct
+    type ('i, 'o, 'e) t = ('i, 'o, 'e) X.t
+  end
 |}
+
 
 let shims_let_op_post_408 =
   {|
+  module type S = sig
+    type ('i, 'o, 'e) t
+    val ( let+ ) : ('i, 'a, 'e) t -> ('a -> 'b) -> ('i, 'b, 'e) t
+    val ( and+ ) : ('i, 'a, 'e) t -> ('i, 'b, 'e) t -> ('i, 'a * 'b, 'e) t
+    val ( let* ) : ('i, 'a, 'e) t -> ('a -> ('i, 'b, 'e) t) -> ('i, 'b, 'e) t
+    val ( and* ) : ('i, 'a, 'e) t -> ('i, 'b, 'e) t -> ('i, 'a * 'b, 'e) t
+  end
+
   module Make(X:sig
     type ('i, 'a, 'e) t
     val (>|=) : ('i, 'a, 'e) t -> ('a -> 'b) -> ('i, 'b, 'e) t
     val monoid_product : ('i, 'a, 'e) t -> ('i, 'b, 'e) t -> ('i, ('a * 'b), 'e) t
     val (>>=) : ('i, 'a, 'e) t -> ('a -> ('i, 'b, 'e) t) -> ('i, 'b, 'e) t
-  end) = struct
+  end) : S with type ('i, 'o, 'e) t = ('i, 'o, 'e) X.t = struct
+    type ('i, 'o, 'e) t = ('i, 'o, 'e) X.t
     let (let+) = X.(>|=)
     let (and+) = X.monoid_product
     let (let*) = X.(>>=)
