1cf35a3922f4489b7ed6f09d3612361a7e3e9b53
Ben Bellick <benbellick@protonmail.com>
2025-01-13 21:51:45 -0600

    test for tupleN and empty_list

--- test-yojson/main.ml
+++ test-yojson/main.ml
@@ -172,8 +172,8 @@ let yojson_basic_suite =
             Format.asprintf "@,@[%a@]" pp_error e )
   in
 
-  let tuple_helper_test =
-    "Tuple helper"
+  let tupleN_test =
+    "TupleN"
     >:: fun _ ->
     let module M = struct
       type t2 = int * string
@@ -181,13 +181,37 @@ let yojson_basic_suite =
       let t2_to_string ((i, s) : t2) = Printf.sprintf "(%i, %s)" i s
     end in
     let open M in
-    let open TupleHelper in
     decoder_test
       ()
       ~decoder:(tuple2 int string)
       ~input:{|[149, "my string"]|}
       ~expected:(149, "my string")
       ~printer:t2_to_string
+  in
+  let empty_list_test =
+    "empty_list"
+    >:: fun _ ->
+    decoder_test
+      ()
+      ~decoder:empty_list
+      ~input:{|[]|}
+      ~expected:()
+      ~printer:(fun () -> Printf.sprintf "()")
+  in
+  let empty_list_nonempty_test =
+    "empty_list_enforced"
+    >:: fun _ ->
+    let input = {|[1, 2]|} in
+    let expected_error =
+      let open Decoders in
+      Error.make ~context:(`List [ `Int 1; `Int 2 ]) "Expected an empty list"
+    in
+    match decode_string empty_list input with
+    | Ok _ ->
+        assert_string "Expected an error"
+    | Error error ->
+        assert_equal expected_error error ~printer:(fun e ->
+            Format.asprintf "@,@[%a@]" pp_error e )
   in
 
   "Yojson.Basic"
@@ -197,7 +221,9 @@ let yojson_basic_suite =
        ; mut_rec_test
        ; string_or_floatlit_test
        ; grouping_errors_test
-       ; tuple_helper_test
+       ; tupleN_test
+       ; empty_list_test
+       ; empty_list_nonempty_test
        ]
 
 
